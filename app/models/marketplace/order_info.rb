aa64a5350b4952d2356c5f0194447f340ee79962
AlexandrT <leshrac88@gmail.com>
2014-08-13 09:09:39 +0000

    First model

--- app/models/marketplace/order_info.rb
+++ app/models/marketplace/order_info.rb
@@ -2,11 +2,23 @@ class OrderInfo
   include Mongoid::Document
 
   field :original_id
-  field :common_info, type: Hash, default ->(){Hash.new}
-  field :contact_info, type: Hash, default ->(){Hasn.new}
-  field :proc_info, type: Hash, default ->(){Hash.new}
-  field :max_cost, type: Hash, default ->(){Hash.new}
-  field :object_info, type: Hash, default ->(){Hash.new}
-  field :participant_requirements, type: Hash, default ->(){Hash.new}
-  field :customer_requirements, type: Hash, default ->(){Hash.new}
+  embeds_one :common_info
+  embeds_one :contact_info
+  embeds_one :proc_info
+  embeds_one :max_cost
+  embeds_one :object_info
+  embeds_one :participant_requirement
+  embeds_one :customer_requirement
+end
+
+class CommonInfo
+  include Mongoid::Document
+
+  field :type
+  field :platform_name
+  field :platform_address
+  field :customer_name
+  field :step
+  field :relation_with_plan
+  embedded_in :order_info
 end
