ca925ec72dbdd5a80400102f68f7f9cc60086cd1
AlexandrT <klipmash@gmail.com>
2014-11-11 00:41:42 +0400

    nothing to commit

--- app/models/marketplace/task.rb
+++ app/models/marketplace/task.rb
@@ -5,5 +5,7 @@ module Marketplace
     field :start_date, type: DateTime
     field :end_date, type: DateTime
     field :result, type: Boolean
+    field :order_type
+    field :count, type: Integer
   end
 end
