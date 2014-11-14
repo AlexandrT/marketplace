bbdd58d42ece9f880c64d021d2bce46c5d05f9d9
Alexandr T <leshrac88@gmail.com>
2014-11-14 12:25:07 +0400

    Tune rabbitmq recipe

--- dev/recipes/cookbooks/main/recipes/rabbitmq.rb
+++ dev/recipes/cookbooks/main/recipes/rabbitmq.rb
@@ -1,1 +1,23 @@
 include_recipe "rabbitmq"
+
+node['rabbitmq']['enabled_plugins'] = ['plugin_management']
+
+rabbitmq_user "guest" do
+  action :delete
+end
+
+rabbitmq_user "amigo" do
+  password "42Amigo_Rabbit"
+  action :add
+end
+
+rabbitmq_user "amigo" do
+  vhost "/"
+  permissions ".* .* .*"
+  action :set_permissions
+end
+
+rabbitmq_user "amigo" do
+  tag "administrator"
+  action :set_tags
+end
