099dc628a0f66eaea88b70be41407d193450dce4
Alexander Tkachev <klipmash@gmail.com>
2015-07-17 17:51:17 +0300

    Fix get_ids tests. [ci skip]

--- spec/bot/parsers/list_parser_spec.rb
+++ spec/bot/parsers/list_parser_spec.rb
@@ -20,24 +20,47 @@ describe Marketplace::Bot::Parsers::ListParser do
   context "get_ids from page with more than 1000" do
     let(:page_more_1000) { File.open(Rails.root + '../../spec/support/order_list_more_1000.html') }
 
+    let(:list_params) do
+      $list_params = { :start_price => start_price, :end_price => end_price, :page => page_more_1000 }
+    end
+
+    overload_amqp
+    let!(:mq) { MQ.new }
+    let!(:topic) { mq.topic("common") }
+    let!(:queue_load) { mq.queue("load") }
+    let!(:queue_parse) { mq.queue("parse") }
+
+    after(:each) do
+      reset_broker
+    end
+
     #before do
     #  producer.should_receive(:load_list).exactly(2).and_return(:producer)
     #end
 
     it "check size and elements of array" do
-      expect(list_parser.get_ids(page_more_1000, start_price, end_price)).to eq(:producer)
+      queue_load.bind(topic, key: "*.load")
+      queue_parse.bind(topic, key: "*.parse")
+      producer.stub(:x).and_return(topic)
+
+      list_parser.get_ids(list_params)
     end
   end
 
   context "get_ids from page without arrow" do 
     let(:page_without_arrow) { File.open(Rails.root + '../../spec/support/order_list_without_arrow.html') }
 
+    let(:list_params) do
+      $list_params = { :start_price => start_price, :end_price => end_price, :page => page_without_arrow }
+    end
+
     #before do
     #  producer.should_receive(:load_order).exactly(10).and_return(:producer)
     #end
 
     it "check call method" do 
-      expect(list_parser.get_ids(page_without_arrow, start_price, end_price)).to include(
+
+      expect(list_parser.get_ids(list_params)).to include(
         "/epz/order/notice/printForm/view.html?regNumber=0320100036714000024",
         "/epz/order/notice/printForm/view.html?regNumber=0122100000314000068",
         "/epz/order/notice/printForm/view.html?regNumber=0361200014514000011",
